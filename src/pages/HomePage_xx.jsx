dd2f77d73238a750940390786104494b0f1764ea
htchung <htchung@gms.tku.edu.tw>
2025-10-15 20:37:41 +0800

    add tailwindcss support

--- src/pages/HomePage_xx.jsx
+++ src/pages/HomePage_xx.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage_xx = () => {
   return (
-    <div>
-      <h1>HomePage_xx</h1>
-      <h2>htchung, 123456789</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-purple-700'>HomePage_xx</h1>
+      <h2 className='text-xl'>htchung, 123456789</h2>
     </div>
   );
 };
